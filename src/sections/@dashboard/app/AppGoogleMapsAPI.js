1ccdd91d221e87ae5fc7757b4fed46a9aa4ddc85
Ryan Jacobs <Ryan.jacobs.739@my.csun.edu>
2022-12-05 19:33:25 -0800

    TRIP-57 removed some items from the navbar, and redid the Dashboard to connect to Google Maps API and provide a map for the demo.

---
+++ src/sections/@dashboard/app/AppGoogleMapsAPI.js
@@ -0,0 +1,56 @@
+import PropTypes from 'prop-types';
+// import ReactApexChart from 'react-apexcharts';
+// @mui
+import { Box, Card, CardHeader } from '@mui/material';
+// Google Maps API
+import {
+    GoogleMap,
+    MarkerF,
+    useLoadScript,
+} from "@react-google-maps/api";
+// useMemo
+import { useMemo } from "react";
+import "./map.css";
+
+// ----------------------------------------------------------------------
+
+AppGoogleMapsAPI.propTypes = {
+    title: PropTypes.string,
+    subheader: PropTypes.string,
+};
+
+function Map() {
+    const center = useMemo(() => ({ lat: 34.2407, lng: -118.5300 }), []);
+
+    return (
+        <GoogleMap zoom={17} center={center} mapContainerClassName="maps-container">
+            <MarkerF
+                title={"CSUN"}
+                name={"CSUN"}
+                key={1}
+                position={center}
+            />
+        </GoogleMap>
+    );
+}
+
+function MapComponent() {
+    const { isLoaded } = useLoadScript({
+        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    });
+
+    if (!isLoaded) return <div>Loading...</div>;
+
+    return <Map />;
+}
+
+export default function AppGoogleMapsAPI({ title, subheader }) {
+    return (
+        <Card>
+            <CardHeader title={title} subheader={subheader} />
+            <Box sx={{ mx: 3, my: 3 }} dir="ltr">
+                <MapComponent />
+            </Box>
+        </Card>
+    );
+}
