b6b9540aa12612ba4097e95ba61715b2c0bb02f5
Natalie Wharton <natwharton17@gmail.com>
2023-04-11 19:42:52 -0700

    TRIP-60 made a fetch request to call the backend to get the logedin user info to display on the profile page

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,3 +1,4 @@
+import { useEffect, useState} from "react";
 import { Helmet } from 'react-helmet-async';
 // @mui
 import {
@@ -8,17 +9,41 @@ import {
 
 
 export default function ProfilePage() {
-  return (
-    <>
-      <Helmet>
-        <title> Profile | Tripper </title>
-      </Helmet>
+    const [data, setData] = useState('');
 
-      <Container>
-        <Card>
-          Coming Soon...
-        </Card>
-      </Container>
-    </>
-  );
+    useEffect(() => {
+        fetch('http://localhost:8080/api/v1/accounts/profile/', {
+            credentials: 'include',
+        })
+            .then((res) => res.json())
+            .then(data => {
+                setData(data);
+            })
+            .catch(err => console.log(err));
+    }, []);
+
+    return (
+        <>
+            <Helmet>
+                <title> Profile | Tripper </title>
+            </Helmet>
+
+            <Container>
+                <Card>
+                    {data ? (
+                        <>
+                            <div>User ID: {data.id}</div>
+                            <div>Username: {data.username}</div>
+                            <div>First Name: {data.first_name}</div>
+                            <div>Last Name: {data.last_name}</div>
+                            <div>Email: {data.email}</div>
+                            {/* Display other profile data here */}
+                        </>
+                    ) : (
+                        <div>Loading profile data...</div>
+                    )}
+                </Card>
+            </Container>
+        </>
+    );
 }
